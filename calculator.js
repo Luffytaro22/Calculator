2aa9349b830b5cca8703904e0d4e565530d45d6e
Luffytaro22 <manuelsanchez059@gmail.com>
2023-04-17 14:20:10 -0500

    Added two functions, handler and buttonClickhandler in order to remove the listener when the key = is press

--- calculator.js
+++ calculator.js
@@ -10,37 +10,52 @@ let buttons = document.querySelectorAll('button');
 let numbers = [];
 let operators = [];
 let auxiliarOutput = "";
-let result;
-buttons.forEach((button) => button.addEventListener('click', () =>{
-    if(button.value === "ce"){
-        output.value = output.value.substring(0, output.value.length-1);
-        numbers.pop();
-    }
-    if(parseFloat(button.value) || button.value === "0" || button.value === "."){
-        output.value = output.value.concat(button.value);
-    }else {
-        if(button.value === "ce" || button.value === "clear"){
-            output.value = output.value;
-        }else{
-            operators.push(button.value);
-            numbers.push(Number(output.value.replace(auxiliarOutput, "")));
-            output.value = output.value.concat(button.value);
-            auxiliarOutput = output.value;
-            if(numbers.length >= 2){
-                result = operate(operators[0], numbers[0], numbers[1]);
-                numbers.shift();
-                numbers.shift();
-                numbers.unshift(result);
-                operators.shift();
-            }
-            if(button.value === "="){
-                output.value = numbers[0]; return;
-            }
+let result, last;
+
+function buttonClickHandler(button) {
+    if (button.value === "ce") {
+      last = output.value.charAt(output.value.length - 1);
+      if (parseFloat(last) || last === "0" || last === ".") {
+        numbers[numbers.length - 1] = Math.floor(numbers[numbers.length - 1] / 10);
+        if (numbers[numbers.length - 1] == 0) {
+          numbers.pop();
         }
-        
+      } else {
+        operators.pop();
+      }
+      output.value = output.value.substring(0, output.value.length - 1);
     }
-    
-}));
+    if (parseFloat(button.value) || button.value === "0" || button.value === ".") {
+      output.value = output.value.concat(button.value);
+    } else {
+      if (button.value === "ce" || button.value === "clear") {
+        output.value = output.value;
+      } else {
+        operators.push(button.value);
+        numbers.push(Number(output.value.replace(auxiliarOutput, "")));
+        output.value = output.value.concat(button.value);
+        auxiliarOutput = output.value;
+        if (numbers.length >= 2) {
+          result = operate(operators[0], numbers[0], numbers[1]);
+          numbers.shift();
+          numbers.shift();
+          numbers.unshift(result);
+          operators.shift();
+        }
+        if (button.value === "=") {
+          output.value = numbers[0];
+          buttons.forEach((button) => button.removeEventListener('click', handler));
+        }
+      }
+    }
+  }
+  
+  function handler(event) {
+    buttonClickHandler(event.target);
+  }
+  
+  buttons.forEach((button) => button.addEventListener('click', handler));
+  
 
 function operate(operator, firstNumber, secondNumber){
     switch(operator){
